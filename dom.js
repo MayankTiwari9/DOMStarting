1da5759f0604d3b144af66a4b6403da15360da98
MayankTiwari9 <mt1027319@gmail.com>
2023-10-09 23:20:36 +0530

    li tag

--- dom.js
+++ dom.js
@@ -117,5 +117,14 @@
 var secondItem = document.querySelector('.list-group-item:nth-child(2)');
 secondItem.style.backgroundColor = 'green';
 
-var thirdItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.display = 'none  ';
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display = 'none  ';
+
+var second = document.querySelectorAll('.list-group-item');
+second[1].style.color = 'green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)'); 
+
+for(var i = 0; i<odd.length;i++){
+  odd[i].style.backgroundColor = 'green';
+}
